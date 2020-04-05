7a4e90d16f356e14a45ba46ed94df4a6bd0ee167
emmanuel <emma.maidana@gmail.com>
2020-04-05 12:10:54 -0300

    react-native-video

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {
+  View,
   Text,
 } from 'react-native';
 
@@ -8,6 +9,7 @@ import Header from './src/section/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import API from './utils/api';
 import CategoryList from './src/videos/containers/category-list';
+import Video from 'react-native-video';
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -27,12 +29,25 @@ export default class App extends Component<Props> {
     return (
       <Home>
         <Header />
+        <View style={{ flex:1, height:100 }}>
+          <Video
+            source={{ uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' }}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              bottom: 0,
+              top: 0
+            }}
+            resizeMode="contain"
+          />
+        </View>
         <Text>buscador</Text>
         <CategoryList
-          list = {this.state.CategoryList} 
+          list={this.state.CategoryList}
         />
-        <SuggestionList 
-          list = {this.state.SuggestionList}
+        <SuggestionList
+          list={this.state.SuggestionList}
         />
       </Home>
     );
